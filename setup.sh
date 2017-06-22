538fbf792f275dd98ecac4db940986420e7e9e02
Danfei Xu <danfei@1101-desktop.corp.zooxlabs.com>
2017-06-22 11:15:25 -0700

    color theme, plugged

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cp -r colors ~/.vim/
+
+echo 'run :PlugInstall in vim'
